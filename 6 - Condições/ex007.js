cc1af19f9e2c663e56678a9a2e8f6157ca41a8b4
Lucas <lucasena020@gmail.com>
2026-07-20 23:10:15 -0300

    feat: adicionando novos exercicios

---
+++ 6 - Condições/ex007.js
@@ -0,0 +1,9 @@
+console.log("O console está funcionando corretamente!");
+
+var velocidade = 50;
+
+if (velocidade >= 80) {
+    console.log("Você ultrapassou a velocidade permitida. MULTADO!");
+}else{
+    console.log("Você está dentro do limite de velocidade permitido.");
+}
